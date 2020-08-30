ce27e48ae41bef9465050511c2ceb14d738fa5c9
UlisesSG <ukisessg@gmail.com>
2020-08-30 04:45:04 -0500

    Primera configuración de webpack

---
+++ src/pages/index/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<a href="./sections/sistema.html">a</a>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,81 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
-  // entry: './src/index.js',
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: 'main.js',
-  // },
-  // resolve: {
-  //   extensions: ['.js'],
-  // },
+  entry: {
+    components: path.resolve(__dirname, 'src/pages/components/components.js'),
+    foundations: path.resolve(__dirname, 'src/pages/foundations/foundations.js'),
+    index: path.resolve(__dirname, 'src/pages/index/index.js'),
+    iterations: path.resolve(__dirname, 'src/pages/iterations/iterations.js'),
+    system: path.resolve(__dirname, 'src/pages/system/system.js'),
+    values: path.resolve(__dirname, 'src/pages/values/values.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.styl'],
+  },
   module: {
     rules: [
       {
-        test: /.pug$/,
-        use: ['pug-loader'],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    ]
+      {
+        test: /\.styl$/,
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
+      },
+    ],
   },
   plugins: [
+    new HotModuleReplacementPlugin(),
+
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'index.pug'),
-      filename: 'index.html'
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'system.pug'),
-      filename: 'sistema.html'
+      template: path.resolve(__dirname, 'public/components.html'),
+      filename: 'sections/componentes.html',
+      chunks: ['components'],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'components.pug'),
-      filename: 'componentes.html'
+      template: path.resolve(__dirname, 'public/foundations.html'),
+      filename: 'sections/fundamentos.html',
+      chunks: ['foundations'],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'foundations.pug'),
-      filename: 'foundations.html'
+      template: path.resolve(__dirname, 'public/iterations.html'),
+      filename: 'sections/iteraciones.html',
+      chunks: ['iterations'],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'iteration.pug'),
-      filename: 'iteracion.html'
+      template: path.resolve(__dirname, 'public/system.html'),
+      filename: 'sections/sistema.html',
+      chunks: ['system'],
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src/pages', 'values.pug'),
-      filename: 'principios.html'
+      template: path.resolve(__dirname, 'public/values.html'),
+      filename: 'sections/valores.html',
+      chunks: ['values'],
     }),
-  ]
-}
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'src/pages'),
+    watchContentBase: true,
+    port: 2001,
+    open: true,
+    index: 'index.html',
+    watchOptions: {
+      poll: 420,
+    },
+  },
+};
