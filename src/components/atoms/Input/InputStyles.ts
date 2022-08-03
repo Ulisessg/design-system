fe976dc3384f6f1419e35ededac9242ae720d27b
mrx <mrx@x.com>
2022-08-02 20:51:14 -0500

    Inputs styles fixed

--- src/components/atoms/Input/InputStyles.ts
+++ src/components/atoms/Input/InputStyles.ts
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
 const InputStyles = styled.input<{ border: boolean }>`
-  width: 200px;
   height: 30px;
   padding-left: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-top: 10px;
   border: ${({ border, theme }) =>
     border && `1.5px solid ${theme.colors.dark2}`};
-  border-radius: 5px;
 `;
 
 export const LabelStyles = styled.label<{ htmlFor: string }>`
   display: grid;
+  width: 100%;
 `;
 
 export default InputStyles;
