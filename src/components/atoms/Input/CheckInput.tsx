13cebd2350c2f1d7205f5990f9e99ba5474e2efc
mrx <mrx@x.com>
2022-08-05 22:16:55 -0500

    Check Input styles fixed

--- src/components/atoms/Input/CheckInput.tsx
+++ src/components/atoms/Input/CheckInput.tsx
@@ -1,7 +1,12 @@
 import React, { InputHTMLAttributes } from "react";
+import styled from "styled-components";
 
 export default function CheckInput(props: CheckInputProps) {
-  return <input {...props} type="checkbox" />;
+  return <CheckInputStyles {...props} type="checkbox" />;
 }
+
+const CheckInputStyles = styled.input<CheckInputProps>`
+  cursor: pointer;
+`;
 
 type CheckInputProps = InputHTMLAttributes<HTMLInputElement>;
