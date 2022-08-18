d958d125304cd0302009a80149eccc155135bfd3
mrx <mrx@x.com>
2022-08-18 16:54:17 -0500

    Video component added

---
+++ src/components/atoms/Video.tsx
@@ -0,0 +1,15 @@
+import React, { VideoHTMLAttributes } from "react";
+import styled from "styled-components";
+
+export default function Video(props: VideoProps) {
+  return <VideoStyles controls {...props} />;
+}
+
+type VideoProps = VideoHTMLAttributes<HTMLVideoElement>;
+
+const VideoStyles = styled.video`
+  margin: 70px;
+  width: 80%;
+  cursor: pointer;
+  box-shadow: 0px 7px 10px 0px ${({ theme }) => theme.colors.shadow};
+`;
