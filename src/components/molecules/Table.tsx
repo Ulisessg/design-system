63dbb42b57174d6af9f035bf60bdca906a2e0837
Ulises Antonio Samano Galvan <ukisessg@gmail.com>
2022-08-22 18:52:32 -0500

    Basic Table component added

---
+++ src/components/molecules/Table.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+export default function Table({ caption, children }: TableProps) {
+  return (
+    <TableStyles>
+      <caption>{caption}</caption>
+      {children}
+    </TableStyles>
+  );
+}
+
+const TableStyles = styled.table`
+  width: 100%;
+  margin-bottom: 15px;
+  border: 1px solid black;
+  padding: 15px;
+  border-collapse: collapse;
+  border-style: hidden;
+
+  caption {
+    margin: 10px 0px;
+    padding-left: 8px;
+    font-weight: bold;
+  }
+
+  thead {
+    tr {
+      border-bottom: 2px solid black;
+    }
+  }
+
+  tr {
+    border-bottom: 1px solid black;
+  }
+
+  th,
+  td {
+    padding: 20px;
+  }
+
+  tfoot {
+    text-align: right;
+  }
+`;
+
+interface TableProps {
+  children: ReactNode;
+  caption: string;
+}
