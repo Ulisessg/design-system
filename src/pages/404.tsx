b5f29228a66df8d012675cac2a03679baf3821b1
Ulises Antonio <ukisessg@gmail.com>
2022-11-07 19:56:32 -0600

    404 page footer fixed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
-import GlobalStyles from "../components/pages/GlobalStyles";
 import { Container } from "../components/pages/404Styles";
 import Link from "../components/atoms/Link";
 
 const NotFound: NextPage = () => {
   return (
     <>
-      <GlobalStyles>
         <Container>
           <img src="/Cheems.png" alt="Cheems" width="350px" />
           <h1>Página no emcontrada</h1>
           <Link href="/" text="Regresar a la página principal" />
         </Container>
-      </GlobalStyles>
     </>
   );
 };
