6e52f24ccbe3789ea36dc257da5ab19e0a6cc375
Ulises Antonio <ukisessg@gmail.com>
2022-11-08 18:47:53 -0600

    Link prop 'text' error fixed

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,10 +1,10 @@
-import React, { AnchorHTMLAttributes } from "react";
-import LinkNext, { LinkProps } from "next/link";
-import styled from "styled-components";
+import React, { AnchorHTMLAttributes } from "react"
+import LinkNext, { LinkProps } from "next/link"
+import styled from "styled-components"
 
-export default function Link(props: LinkComponentProps) {
+export default function Link (props: LinkComponentProps) {
   return (
-   <>
+    <>
       {props.target ? (
         <LinkNextStyles {...props}>{props.text}</LinkNextStyles>
       ) : (
@@ -16,34 +16,34 @@ export default function Link(props: LinkComponentProps) {
           {props.text}
         </LinkNextStyles>
       )}
-      </>
-  );
+    </>
+  )
 }
 
-interface LinkExternalProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+interface LinkExternalProps extends AnchorHTMLAttributes<HTMLAnchorElement> { }
 
 interface LinkComponentProps
   extends LinkProps,
-    Omit<
-      LinkExternalProps,
-      "href" | "onClick" | "onMouseEnter" | "onTouchStart"
-    > {
-  text: string;
-  as?: any;
-  version?: "lighter" | "darker";
-  target?: "_blank" | "_self" | "_parent" | "_top";
+  Omit<
+    LinkExternalProps,
+    "href" | "onClick" | "onMouseEnter" | "onTouchStart"
+  > {
+  text: string
+  as?: any
+  version?: "lighter" | "darker"
+  target?: "_blank" | "_self" | "_parent" | "_top"
 }
 
-const LinkNextStyles = styled(LinkNext)<LinkComponentProps>`
+const LinkNextStyles = styled(LinkNext) <LinkComponentProps>`
   color: ${({ version, theme }) => {
-    if (version === "lighter") return;
-    return theme.colors.dark1;
+    if (version === "lighter") return
+    return theme.colors.dark1
   }};
   :active,
   :focus {
     color: ${({ theme, version }) => {
-      if (version === "lighter") return theme.colors.dark1;
-      return theme.colors.dark2;
-    }};
+    if (version === "lighter") return theme.colors.dark1
+    return theme.colors.dark2
+  }};
   }
-`;
+`
