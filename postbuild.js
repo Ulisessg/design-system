3cdbd847ed902f487d87458aa93d1e98adaebc26
Ulises Antonio <ukisessg@gmail.com>
2022-11-08 20:48:34 -0600

    Theme definition file added in bundle

--- postbuild.js
+++ postbuild.js
@@ -1,15 +1,17 @@
-const { readFile, writeFile, unlink, readdirSync } = require("fs");
-const { resolve } = require("path");
-const { cwd } = require("process");
+const { readFile, writeFile, unlink, copyFileSync } = require("fs")
+const { resolve } = require("path")
+const { cwd } = require("process")
 
 readFile(resolve(cwd(), "dist", "index.js"), (err, data) => {
   if (err) throw err;
   (async () => {
     const newFile = `#!/usr/bin/env node
 ${data}
-  `;
+  `
     unlink(resolve(cwd(), "dist", "index.js"), () => {
-      writeFile(resolve(cwd(), "dist", "index.js"), newFile, () => {});
-    });
-  })();
-});
+      writeFile(resolve(cwd(), "dist", "index.js"), newFile, () => { })
+    })
+  })()
+})
+
+copyFileSync(resolve(__dirname, 'src', 'styled.d.ts'), resolve(__dirname, 'dist', 'Theme.d.ts'))
