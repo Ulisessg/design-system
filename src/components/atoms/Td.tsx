fd0af4f01870982dee8c3188a11eb731c9fed4a7
Ulises Antonio <ukisessg@gmail.com>
2022-11-17 17:10:14 -0600

    Td component added

---
+++ src/components/atoms/Td.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const Td = styled.td`
+text-align: center;
+`
+
+export default Td
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as Button } from "./components/atoms/Button";
 export { default as Link } from "./components/atoms/Link";
 export { default as LoadingSpinner } from "./components/atoms/LoadingSpinner";
 export { default as Video } from "./components/atoms/Video";
+export { default as Td } from './components/atoms/Td'
 
 // Molecules
 export * from "./components/molecules/Form";
