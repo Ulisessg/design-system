296ec6847ff6765530e632ad4cc486965b27148a
Ulises Antonio <ukisessg@gmail.com>
2022-12-06 23:01:32 -0600

    Text Input styles fixed

--- src/components/atoms/Input/InputStyles.ts
+++ src/components/atoms/Input/InputStyles.ts
@@ -2,20 +2,25 @@ import styled from "styled-components";
 
 const borderPixels = "1.8px";
 
-const InputStyles = styled.input<{ border: boolean }>`
+const InputStyles = styled.input<{ border: boolean, checkInvalid?: boolean }>`
   height: 30px;
   padding-left: 10px;
   border-radius: 5px;
   margin-bottom: 10px;
   margin-top: 10px;
-
+  border: ${borderPixels} solid ${({theme}) => theme.colors.dark2};
   &:valid {
     border: ${({ border, theme }) =>
       border && `${borderPixels} solid ${theme.colors.dark2}`};
   }
 
   &:invalid {
-    border: ${borderPixels} solid ${({ theme }) => theme.colors.error};
+    border: ${borderPixels} solid ${({ theme, checkInvalid }) => {
+      if(checkInvalid === true) {
+        return theme.colors.error
+      }
+      return theme.colors.dark2
+    }};
     outline: none;
   }
 `;
--- src/components/atoms/Input/TextInput.tsx
+++ src/components/atoms/Input/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, FocusEvent, ReactElement, useState } from "react";
 import InputStyles, { LabelStyles } from "./InputStyles";
 import { TextInputProps } from "./InputProps";
 
@@ -13,10 +13,25 @@ import { TextInputProps } from "./InputProps";
  * @return {import('react').ReactElement} ReactElement
  */
  const TextInput: FC<TextInputProps> = (props: TextInputProps): ReactElement => {
+  const [checkInvalid, setCheckInvalid] = useState<boolean>(false)
+  const handleFocus = (ev: FocusEvent<HTMLInputElement>) => {
+    if(props.onFocus) props.onFocus(ev)
+    if(checkInvalid === true) return
+    setCheckInvalid(true)
+  }
+  
+
   return (
     <LabelStyles htmlFor={props.id}>
       {props.label}
-      <InputStyles  {...props}  border={true} placeholder={props.placeholder} type={props.type} />
+      <InputStyles  
+        {...props}
+        border={true}
+        placeholder={props.placeholder}
+        type={props.type}
+        onFocus={handleFocus}
+        checkInvalid={checkInvalid}
+      />
     </LabelStyles>
   );
 }
