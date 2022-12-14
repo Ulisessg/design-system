8463db2364c2cffeb249fb555e153d9e72fce7d5
Ulises Antonio <ukisessg@gmail.com>
2022-12-13 21:37:59 -0600

    Test page fixed

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import type { NextPage } from "next";
-import GlobalStyles from "../components/pages/GlobalStyles";
 
 const Test: NextPage = () => {
   if (process.env.NODE_ENV === "production") return <p>Test</p>;
 
   return (
-    <GlobalStyles>
-      <p>Test</p>
-    </GlobalStyles>
+    <>
+      <p>Test</p>;
+    </>
   );
 };
 
