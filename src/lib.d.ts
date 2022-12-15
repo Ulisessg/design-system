a564fb11e7986da18a1a040301a3002592b71175
Ulises Antonio <ukisessg@gmail.com>
2022-12-15 02:19:05 -0600

    Components props fixed

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,3 +1,3 @@
 import {ComponentPropsWithRef, HTMLAttributes} from 'react'
 
-declare export type ComponentProps<El extends keyof JSX.IntrinsicElements> = ComponentPropsWithRef<El> & HTMLAttributes<El> & {}
+declare export type ComponentProps<El extends keyof JSX.IntrinsicElements> = ComponentPropsWithRef<El> & {}
