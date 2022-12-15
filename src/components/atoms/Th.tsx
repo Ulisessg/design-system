86ed010143d6957f9db40539276490c7581cabbe
Ulises Antonio <ukisessg@gmail.com>
2022-12-14 22:55:58 -0600

    Th component props improved

--- src/components/atoms/Th.tsx
+++ src/components/atoms/Th.tsx
@@ -1,7 +1,11 @@
+import { forwardRef } from 'react'
 import styled from 'styled-components'
+import { ComponentProps } from '../../lib'
 
-const Th = styled.th`
+const ThStyles = styled.th`
   text-align: center;
 `
 
-export default Th
+export default forwardRef<HTMLTableCellElement, ComponentProps<'th'>>(function Th(props, ref) {
+  return <ThStyles {...props} ref={ref}>{props.children}</ThStyles>
+})
