619f2c6a3448798a170889be1af1fc92dfbcc948
Ulises Antonio <ukisessg@gmail.com>
2023-01-28 21:55:28 -0600

    Types in build fixed

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from "@rollup/plugin-typescript"
 import terser from "@rollup/plugin-terser"
 import peerDepsExternal from "rollup-plugin-peer-deps-external"
 import packageJson from "./package.json"
+import dts from 'rollup-plugin-dts'
 
 /** @type {import('rollup').RollupOptions[]}*/
 const exp = [
@@ -26,6 +27,11 @@ const exp = [
       typescript({ tsconfig: "./tsconfig-build.json" })
     ],
     external: ["react", "react-dom", "styled-components"],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    plugins: [dts.default()],
   }
 ]
 
