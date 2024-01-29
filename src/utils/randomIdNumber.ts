ecbe8ebf471542550bbe9722f952ed3db18a8d83
Ulises Antonio <ukisessg@gmail.com>
2024-01-29 01:43:25 -0600

    DS-4/Package - 'randomIdNumber' util deprecated

--- src/utils/randomIdNumber.ts
+++ src/utils/randomIdNumber.ts
@@ -1,3 +1,7 @@
+/**
+ * @deprecated This method should be replaced with 'useId' react hook, maintained for compatibility purpose
+ * @returns {string}
+ */
 export default function randomIdNumber(): string {
   return `${Math.floor(Math.random() * 1200 * Math.random() * 100)}`;
 }
