9cba37d604f8bd03e486475e96e5cb2ce9a390af
Ulises Antonio <ukisessg@gmail.com>
2024-02-03 22:21:45 -0600

    DS-4/Storybook - Landing page created

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/index.stories.tsx",
+    "../src/**/*.mdx",
+     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
